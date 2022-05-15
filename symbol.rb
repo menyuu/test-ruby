2cf692df79213ffafd0f683edf8fc5c0ab41e430
yuu <101458830+menyuu@users.noreply.github.com>
2022-05-15 08:21:47 +0000

    rubyの学習 class classメソッド self オーバーライド シンボル

---
+++ symbol.rb
@@ -0,0 +1,31 @@
+webcamp = :プログラミング学習
+puts webcamp
+
+
+ # シンボル
+ :プログラミング学習
+  # 文字列
+ "プログラミング学習"
+ 
+ 
+ # 文字列オブジェクト
+dog1 = "犬"
+dog2 = "犬"
+
+# シンボルオブジェクト
+dog3 = :犬
+dog4 = :犬
+
+dog5 = "いぬ"
+dog6 = "いぬ"
+dog7 = :いぬ
+dog8 = :いぬ
+
+puts dog1.object_id
+puts dog2.object_id
+puts dog3.object_id
+puts dog4.object_id
+puts dog5.object_id
+puts dog6.object_id
+puts dog7.object_id
+puts dog8.object_id
